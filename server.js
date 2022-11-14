3f43dc2c8732c271337ee7472d6e9a0f1d7028f0
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-13 22:52:53 -0500

    JSON data available to view

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 3000
+const drinks = require("./models/drinks")
+const morgan = require("morgan")
+
+app.use(morgan("dev"))
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Listening on planet ${PORT}`)
 })
