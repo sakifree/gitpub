bc26ea54e3f7ef864884cca7496113c242e969ed
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-14 00:33:04 -0500

    creates routes for foods

--- server.js
+++ server.js
@@ -2,28 +2,52 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const drinks = require("./models/drinks")
+const food = require("./models/food")
 const morgan = require("morgan")
 
 app.use(morgan("dev"))
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Gitpub App!")
+    res.send(`
+        Welcome to the Gitpub App! </br> 
+        <a href="http://localhost:${PORT}/drinks">Drinks</a>
+        <a href="http://localhost:${PORT}/food">Food</a>
+    `)
 })
 
 app.get("/drinks", (req, res) => {
     res.render(
         "drinks_index.ejs",
         {
-            allDrinks:drinks
+            allDrinks: drinks,
         }
     )
 })
+
+app.get("/food", (req, res) => {
+    res.render(
+        "food_index.ejs",
+        {
+            allFood: food,
+        }
+    )
+})
+
 
 app.get("/drinks/:id", (req, res) => {
     res.render(
         "drinks_show.ejs",
         {
-            drink: drinks[req.params.id]
+            drink: drinks[req.params.id],
+        }
+    )
+})
+
+app.get("/food/:id", (req, res) => {
+    res.render(
+        "food_show.ejs",
+        {
+            food: food[req.params.id],
         }
     )
 })
